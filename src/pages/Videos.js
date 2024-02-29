35aacfbef2f0af11329af921ee8d4933d53cd9f9
clearup23 <154214761+clearup23@users.noreply.github.com>
2024-02-29 09:31:39 +0530

    Update Videos.js

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -4,10 +4,18 @@ import "./Video.css";
 import Navbar2 from "../layout/Navbar2";
 const Videos = () => {
   const [videos, setVideos] = useState([]);
-
+ 
   const handleClick = (subject) => {
     if (subject === "Maths") {
       setVideos([
+        {
+          title: "Class 1",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhX0yhin2ynxbUciVLaA_5CW",
+        },
+        {
+          title: "Class 2",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhUt-UOLdvVZSszEPuEuokfZ",
+        },
         {
           title: "Class 3",
           url: "https://www.youtube.com/embed/fo6reQIO8VE?list=PLW6ZJtqCeoWvJmbQrhfeLYhebwElIr8w1",
@@ -32,11 +40,30 @@ const Videos = () => {
           title: "Class 8",
           url: "https://www.youtube.com/embed/mZyk1vy953Q?list=PLVLoWQFkZbhWwuZmoe15zj2Kmq7sApfcw",
         },
-
+        {
+          title: "Class 9",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhW1YSpUGOUrqAu7SvJCaZNQ",
+        },
+        {
+          title: "Class 10",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhWtsL4EkoiD2PJyeOBzRmx4",
+        },
+ 
         // Add more videos for other classes
       ]);
     } else if (subject === "Science") {
       setVideos([
+ 
+        {
+          title: "Class 1",
+          url: "https://www.youtube.com/embed/videoseries?list=PLiPy3hM238v7e0PSPe4TcIWcvC7MR3pBQ" ,
+        },
+ 
+        {
+          title: "Class 2",
+          url: "https://www.youtube.com/embed/videoseries?list=PLiPy3hM238v4DqsRMNMonZl0ghikIfSym" ,
+        },
+       
         {
           title: "Class 3",
           url: "https://www.youtube.com/embed/909RZYjfFGE?list=PLW6ZJtqCeoWuYiJDluwn1b_nsNs_LYP7m",
@@ -57,50 +84,110 @@ const Videos = () => {
           title: "Class 7",
           url: "https://www.youtube.com/embed/gDfNuLHVbWQ?list=PLVLoWQFkZbhUN3DiztHOVansWcBhCppjH",
         },
-
+ 
         {
           title: "Class 8",
           url: "https://www.youtube.com/embed/31Xglzp-Eec?list=PLVLoWQFkZbhV8K8NiJrFoMSwa_34UDMdg",
         },
-
+ 
+        {
+          title: "Class 9",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhXMfqtNQm232yBznMitpW8Y" ,
+        },
+ 
+        {
+          title: "Class 10",
+          url: "https://www.youtube.com/embed/videoseries?list=PLv2hpilqtBVkRxWmT1QfAJQIvTrloqE0p" ,
+        },
+       
+ 
         // Add more videos for other classes
       ]);
     } else if (subject === "Social") {
       setVideos([
         {
+          title: "Class 1",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhWuj7L8aJhjmt967YdzewFc" ,
+        },
+ 
+        {
+          title: "Class 2",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhWc8MTj3ij7kvByNXxUc6S8",
+        },
+ 
+        {
           title: "Class 3",
           url: "https://www.youtube.com/embed/7znIVTqN0aY?list=PL2KP15Aw-29XG6LFCnb1DWAmej-UQBySP",
         },
-
+ 
         {
           title: "Class 4",
           url: "https://www.youtube.com/embed/Xea196ugSzQ?list=PLIVxQtdk068GxnlSyI2oZOBM55FCpbFOl&t=21",
         },
-
+ 
         {
           title: "Class 5",
           url: "https://www.youtube.com/embed/7znIVTqN0aY?list=PL2KP15Aw-29XG6LFCnb1DWAmej-UQBySP",
         },
-
+ 
         {
           title: "Class 6",
           url: "https://www.youtube.com/embed/2clQ3zWUIN0?list=PLVLoWQFkZbhX8NJIryAuRqcl9I3xFAZWC",
         },
-
+ 
         {
           title: "Class 7",
           url: "https://www.youtube.com/embed/V0CcWG2LpDQ?list=PLVLoWQFkZbhWvzFnibzJ2jqPiQBu4KCss",
         },
-
+ 
         {
           title: "Class 8",
           url: "https://www.youtube.com/embed/V0CcWG2LpDQ?list=PLVLoWQFkZbhWvzFnibzJ2jqPiQBu4KCss",
         },
-
+       
+        {
+          title: "Class 9 History",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhUvAmP7ZlLrVAfXHuoGthee",
+        },
+ 
+        {
+          title: "Class 9 Geography",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhVnay9XxI3B1G3-6nOsXM2L",
+        },
+ 
+        {
+          title: "Class 9 Civics",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhViA8KrG2BbHPS16cZvBDRF",
+        },
+ 
+        {
+          title: "Class 10 History",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhUqoA8LckeDv_d75e0JaQwL",
+        },
+ 
+        {
+          title: "Class 10 Geography",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhWTMCmKVQENOML-eJilisJz" ,
+        },
+ 
+        {
+          title: "Class 10 Civics",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhUYHbjSjERDjtWNTwuI9ODP" ,
+        },
+ 
+ 
         // Add more videos for other classes
       ]);
     } else if (subject === "English") {
       setVideos([
+        {
+          title: "Class 1",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhUgWG86tMtoodU40io7KFWZ",
+        },
+        {
+          title: "Class 2",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhXIXJBvDCQI36BRbEPY7Vz2",
+        },
         {
           title: "Class 3",
           url: "https://www.youtube.com/embed/LRNUOU2DW_8?list=PLVLoWQFkZbhUb372PfgDFDT5Uu9b5yAJQ",
@@ -125,22 +212,30 @@ const Videos = () => {
           title: "Class 8",
           url: "https://www.youtube.com/embed/v=eqtu1UErZCc?list=PLVLoWQFkZbhU1w-uN42Yp0O1AltvVEVFT",
         },
+        {
+          title: "Class 9",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhWvnzQBSVOj14dJlH6YeoQc",
+        },
+        {
+          title: "Class 10",
+          url: "https://www.youtube.com/embed/videoseries?list=PLVLoWQFkZbhVGeoo-eZSYwbd-zHpjxKSu",
+        },
       ]);
     }
   };
-
+ 
   useEffect(() => {
     // Set default videos when the component mounts
     handleClick("Maths");
   }, []); // Empty dependency array ensures that this effect runs only once on mount
-
+ 
   return (
     <div>
       <Navbar2 />
       {/* <div className="header">
         <h1>VIDEOS</h1>
       </div> */}
-
+ 
       <div className="content">
         <ul className="subject-list">
           <li className="subject" onClick={() => handleClick("Maths")}>
@@ -169,5 +264,5 @@ const Videos = () => {
     </div>
   );
 };
-
+ 
 export default Videos;
